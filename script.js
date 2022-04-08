201ddf31769cb67e395df871867c628d9c67c304
pavanchambhare <pavanchambhare88gmail.com>
2022-04-08 13:28:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+ const age = 12;
+
+if (age => 18) {
+  console.log("Nick is an adult.");
+} else {
+  console.log("Nick is a child.");
+}
+const abc = 17;
+
+if (abc => 24) {
+  console.log("Jerry is old enough to drive and has his own car.");
+} 
+// Primitive data types
+
+// String
+let name = "pavan";
+console.log("My string is " + name);
+console.log("Data type is " + (typeof name));
+
+// Numbers
+let marks = 34.4;
+console.log("Data type is " + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log("Data type is " + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log("Data type is " + (typeof nullVar));
+
+// Undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef));
+
+// Reference Data Types
+
+// Arrays
+let myarr = [1, 2, 3, 4, false, "string"];
+console.log("Data type is " + (typeof myarr));
+
+// Object Literals
+let stMarks = {
+    harry: 89,
+    Shubham: 36,
+    Rohan: 34
+}
+console.log( typeof stMarks);
+
+function findName() {
+    
+}
+console.log( typeof findName);
+
+let date = new Date();
+console.log( typeof date);
+
